b77d995fb39d77c217138966c49c7eed2361a692
Denzel Carter <carter.denzel@yahoo.com>
2014-09-19 18:28:48 -0400

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Denzel Carter on 9/19/14.
+//  Copyright (c) 2014 RushAlert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
